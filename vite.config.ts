a44a4e7d97d02982b77b6d55e4a2fa20a4231ed9
PlanetDestroyyer <wrieddude@gmail.com>
2025-10-24 12:01:13 +0530

    new6

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/assets/*',
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
